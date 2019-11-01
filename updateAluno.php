1a6a52891cc5a449d0b29c5da1271f4c6c5ff0d3
unknown <Digital@DH-A7-30.dhbrasil.digitalhouse.com>
2019-11-01 11:04:50 -0300

    colocar valores dentro dos campus

--- updateAluno.php
+++ updateAluno.php
@@ -8,16 +8,16 @@
 
     //puxar as info do aluno que quero persquisar no banco
     if(isset($_GET['id'])){
-        $id = $GET['id'];
+        $id = $_GET['id'];
     }else{
         echo "voce deve passar um id";
         exit;
     }
 
     $query = $db->prepare('SELECT * from alunos where id=?');
-    $aluno = $query->execute([$id]);
-    $aluno = $aluno->fecth(PDO::FETCH_ASSOC);
-    var_dump($aluno);
+    $query->execute([$id]);
+    $aluno = $query->fetch(PDO::FETCH_ASSOC);
+    //var_dump($aluno);
 ?>
 
 <!DOCTYPE html>
@@ -31,15 +31,19 @@
 <body>
     <form action="cadastroAluno.php" method="post">
         <h2>Nome Aluno</h2>
-        <input type="text" name="nomeAluno">
+        <input type="text" name="nomeAluno" value=<?php echo $aluno['nome']; ?>>
         <h2>RA do Aluno</h2>
-        <input type="text" name="raAluno">
+        <input type="text" name="raAluno" value=<?php echo $aluno['ra']; ?> disabled>
         <!--Criar um select com as info que tem no banco-->
         <h2>Cursos Disponiveis</h2>
         <select name="curso">
             <!--Deixar o option dinamico de acordo com o que eu tenho cadastrado no banco-->
             <?php foreach($cursos as $curso){?>
-                <option value="<?= $curso['id']; ?>"><?php echo $curso['nome']; ?></option>
+                <?php if($curso["id"] == $aluno["curso_id"]){?>
+                <option selected value="<?= $curso['id']; ?>"><?php echo $curso['nome']; ?>
+                    <?php $curso["nome"]?>
+                </option>
+                <?php }?>
             <?php }?>
         </select>
         <button type='submit'>Cadastrar</button>
